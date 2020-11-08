8b0b032761e51f14817501a9fbf041e916891946
Dana Gomez <akameftw13@gmail.com>
2020-11-08 21:41:18 +0100

    Agregar archivos de proyecto.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("CiberpunkDana")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CiberpunkDana")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("395076b8-1b4b-4172-a0bc-0534f5426218")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Persona.vb
@@ -0,0 +1,77 @@
+﻿Public Class Persona
+    Private nom As String
+    Private cognom As String
+    Private edat As Int32
+    Private territori As String
+    Private capacitat As String
+    Private espai As String
+    Private educacio As String
+    Private gremi As String
+
+    Public Sub New(ByVal nom As String, ByVal cognom As String, ByVal edat As Int32, ByVal territori As String, ByVal capacitat As String, ByVal espai As String, ByVal educacio As String, ByVal gremi As String)
+        Me.nom = nom
+        Me.cognom = cognom
+        Me.edat = edat
+        Me.territori = territori
+        Me.capacitat = capacitat
+        Me.espai = espai
+        Me.educacio = educacio
+        Me.gremi = gremi
+    End Sub
+
+    Public Sub setNom(ByVal nom As String)
+        Me.nom = nom
+    End Sub
+    Public Function getNom() As String
+        Return Me.nom
+    End Function
+
+    Public Sub setCognom(ByVal cognom As String)
+        Me.cognom = cognom
+    End Sub
+    Public Function getCognom() As String
+        Return Me.cognom
+    End Function
+
+    Public Sub setEdat(ByVal edat As Int32)
+        Me.edat = edat
+    End Sub
+    Public Function getEdat() As Int32
+        Return Me.edat
+    End Function
+
+    Public Sub setTerritori(ByVal territori As String)
+        Me.territori = territori
+    End Sub
+    Public Function getTerritori() As String
+        Return Me.territori
+    End Function
+
+    Public Sub setCapacitat(ByVal capacitat As String)
+        Me.capacitat = capacitat
+    End Sub
+    Public Function getCapacitat() As String
+        Return Me.capacitat
+    End Function
+
+    Public Sub setEspai(ByVal espai As String)
+        Me.espai = espai
+    End Sub
+    Public Function getEspai() As String
+        Return Me.espai
+    End Function
+
+    Public Sub setEducacio(ByVal educacio As String)
+        Me.educacio = educacio
+    End Sub
+    Public Function getEducacio() As String
+        Return Me.educacio
+    End Function
+
+    Public Sub setGremi(ByVal gremi As String)
+        Me.gremi = gremi
+    End Sub
+    Public Function getGremi() As String
+        Return Me.gremi
+    End Function
+End Class
